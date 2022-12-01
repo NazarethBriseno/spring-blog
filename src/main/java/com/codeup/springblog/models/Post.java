371a355bb86938f8a7d5c2416d5bfaa498edc577
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-12-01 10:08:47 -0600

    Updated One to Many

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,5 +1,6 @@
 package com.codeup.springblog.models;
 
+
 import javax.persistence.*;
 
 @Entity
@@ -10,41 +11,39 @@ public class Post {
     private long id;
     @Column(nullable = false, length = 100)
     private String title;
-    @Column(nullable = false, length = 500)
+    @Column(nullable = false, length = 250)
     private String body;
+    @ManyToOne
+//    @JoinColumn(name="user_id")
+    private User user;
 
+
+    //Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+//Constructors
     public Post(){
     }
     public Post(String title, String body){
         this.title = title;
         this.body = body;
     }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public Post(long id, String title, String body) {
         this.id = id;
         this.title = title;
